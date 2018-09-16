8d3318df2b286789b64ecec3b8e0c11efc0e6c0c
Noel Light-Hilary <noel@noellh.com>
2018-09-16 11:42:41 +0100

    First attempt to add Clipboard support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage';
+import { Clipboard } from '@ionic-native/clipboard';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
   entryComponents: [],
   imports: [
     BrowserModule,
+    Clipboard,
     IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
     AppRoutingModule,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Clipboard } from '@ionic-native/clipboard';
 import makePassword from '@webful/passwordmaker-lib';
 
 import { Input } from '../../models/Input';
@@ -9,17 +11,37 @@ import { SettingsService } from '../settings.service';
   selector: 'app-home',
   templateUrl: 'home.page.html',
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public clipboard_available = false;
   public input: Input = new Input('', '');
   public output_password: string;
 
   constructor(
+    private clipboard: Clipboard,
     private settingsService: SettingsService,
+    public toast: ToastController,
   ) {}
+
+  public ngOnInit() {
+    if (window.cordova && this.clipboard) { // no clipboard in-browser for now
+      this.clipboard_available = true;
+    }
+  }
 
   public updatePassword() {
     return this.settingsService.getCurrentSettings().then(settings => {
       this.output_password = this.makePassword(this.input.master_password, this.input.domain, settings);
+    });
+  }
+
+  public copy() {
+    this.clipboard.copy(this.output_password).then(() => {
+      this.toast.create({
+        message: (`Copied to clipboard!`),
+        duration: 2000,
+        showCloseButton: true,
+        closeButtonText: 'OK',
+      }).then(successToast => successToast.present());
     });
   }
 
