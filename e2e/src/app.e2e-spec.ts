1dd7f785ef3ec3c34be4c45cde3173ef51d1d329
Noel Light-Hilary <noel@noellh.com>
2018-10-21 02:06:50 +0100

    Add e2e tests that cover altering settings

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,7 +7,7 @@ describe('new App', () => {
     page = new AppPage();
   });
 
-  it('should display master password field label', () => {
+  it('should display the master password field label', () => {
     page.navigateToHome();
 
     expect(page.getParagraphText()).toContain('Master password');
@@ -16,10 +16,60 @@ describe('new App', () => {
   it('should generate the right password for the default settings', () => {
     page.navigateToHome();
 
-    page.populateIonicInput('host', 'example.com');
+    page.populateIonicInput('host', 'my.example.com');
     page.populateIonicInput('master_password', 'test');
 
-    // Check for correct output with alphanumeric charset, 15 length, HMAC-SHA256.
-    expect(page.getOutputPassword()).toContain('rJeGcpSWpH36PMn');
+    // Check for correct output for "example.com" with alphanumeric charset, 15 length, HMAC-SHA256.
+    expect(page.getOutputPassword()).toEqual('rJeGcpSWpH36PMn');
+  });
+
+  it('should generate the right password with domain_only switched off', () => {
+    page.navigateToSettings();
+    page.setIonicToggle('domain_only', false);
+    page.save();
+
+    page.navigateToHome();
+
+    page.populateIonicInput('host', 'my.example.com');
+    page.populateIonicInput('master_password', 'test');
+
+    // Check for correct output for "my.example.com" with alphanumeric charset, 15 length, HMAC-SHA256.
+    expect(page.getOutputPassword()).toEqual('RRC4KZEH8mzHfd6');
+  });
+
+  it('should generate the right password with an altered length, characters and algorithm', () => {
+    page.navigateToSettings();
+    page.populateIonicInput('output_length', 25);
+    page.populateIonicSelect('output_character_set', 'Letters').then(() => {
+      page.populateIonicSelect('algorithm', 'SHA1').then(() => {
+        page.setIonicToggle('domain_only', true);
+        page.save();
+
+        page.navigateToHome();
+        page.populateIonicInput('host', 'my.example.com');
+        page.populateIonicInput('master_password', 'test');
+
+        // Check for correct output for "example.com" with letters charset, 25 length, SHA1.
+        expect(page.getOutputPassword()).toEqual('wddqcgQOWiMBbFeTSlsQawkuB');
+      });
+    });
+  });
+
+  it('should generate the right password with an altered length, characters, algorithm and literal text handling', () => {
+    page.navigateToSettings();
+    page.populateIonicInput('output_length', 25);
+    page.populateIonicSelect('output_character_set', 'Letters').then(() => {
+      page.populateIonicSelect('algorithm', 'SHA1').then(() => {
+        page.setIonicToggle('domain_only', false);
+        page.save();
+
+        page.navigateToHome();
+        page.populateIonicInput('host', 'my.example.com');
+        page.populateIonicInput('master_password', 'test');
+
+        // Check for correct output for "my.example.com" with letters charset, 25 length, SHA1.
+        expect(page.getOutputPassword()).toEqual('PSZKuWvFAtVWNoIqfBsAHnEOr');
+      });
+    });
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,24 +1,70 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateToHome() {
+  public navigateToHome() {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  public navigateToSettings() {
+    return browser.get('/tabs/(settings:settings)');
+  }
+
+  public getParagraphText() {
     return element(by.deepCss('app-root ion-content')).getText();
   }
 
-  populateIonicInput(elementName: string, value: string) {
+  public populateIonicInput(elementName: string, value: (string|number)) {
     const ionicInput = element(by.css(`ion-input[name="${elementName}"]`));
 
     // Because some fields use floating input labels, we need to click & wait for a UI update before interacting.
     ionicInput.click();
     browser.waitForAngular();
-    ionicInput.sendKeys(value);
+
+    // `clear()` wasn't allowed on `ion-input` or the native field, so backspace x20 to remove existing value.
+    ionicInput.sendKeys('\b'.repeat(20) + value);
   }
 
-  getOutputPassword() {
+  public populateIonicSelect(elementName: string, valueLabel: string) {
+    const ionicSelect = element(by.css(`ion-select[name="${elementName}"]`));
+    ionicSelect.click();
+    browser.sleep(400); // Wait for elements to be available and visible
+
+    return element.all(by.css('div.alert-radio-group > button')).map(possibleRadio => {
+      possibleRadio.getText().then(possibleRadioText => {
+        if (possibleRadioText === valueLabel) {
+          possibleRadio.click();
+          element(by.buttonText('OK')).click();
+
+          browser.sleep(200); // Wait for overlay's close animation so it doesn't steal focus
+        }
+      }).catch(error => {
+        // If we already found the desired radio button in another elements from the `map` and
+        // clicked OK, this one won't have an element to `getText()` from any more. This is fine
+        // and we just need to catch the WebDriver error so the test can proceed.
+      });
+    });
+  }
+
+  public setIonicToggle(elementName: string, shouldBeChecked: boolean) {
+    const ionicInput = element(by.css(`ion-toggle[name="${elementName}"]`));
+
+    ionicInput.getAttribute('checked').then((currentValue: string) => {
+      const isChecked: boolean = (currentValue === 'true');
+      if (isChecked !== shouldBeChecked) {
+        browser.waitForAngular();
+        ionicInput.click();
+      }
+    });
+  }
+
+  public save() {
+    const ionicSaveButton = element(by.css(`ion-button[name="save"]`));
+    browser.waitForAngular();
+    ionicSaveButton.click();
+  }
+
+  public getOutputPassword() {
+    browser.waitForAngular();
     return element(by.css('span.output_password')).getText();
   }
 }
