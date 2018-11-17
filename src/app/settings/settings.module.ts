ad65f472c2157cec563f961e680fe2b4cfcb6fee
Noel Light-Hilary <noel@noellh.com>
2018-11-17 13:11:43 +0000

    Drop non-reactive `FormsModule`

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -2,14 +2,13 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SettingsPage } from './settings.page';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
-    FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild([{ path: '', component: SettingsPage }])
   ],
