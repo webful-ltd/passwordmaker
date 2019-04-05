010ed5358efddd876568b733ebc8c15e86657759
Noel Light-Hilary <noel@noellh.com>
2019-04-05 12:54:17 +0100

    Preload other routes (i.e. Settings) on launch
    As per
    https://blog.ionicframework.com/how-to-lazy-load-in-ionic-angular/
    this is probably the most sensible behaviour
    for an app this size and should make the first
    load of Settings slightly quicker.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PreloadAllModules, Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
