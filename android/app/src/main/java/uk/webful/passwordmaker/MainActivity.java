d45a0bb53bdc71c3742daade0b8f00d2fbb05054
Noel Light-Hilary <noel@noellh.com>
2022-01-08 12:41:17 +0000

    WIP method #1 adding dark mode support
    Via https://medium.com/@hinddeep.purohit007/supporting-dark-mode-in-ionic-capacitor-8e57fe9dbd47
    and slightly modified fork of the fork discussed at
    https://github.com/hinddeep/ionicCapacitorDarkMode/issues/10

--- android/app/src/main/java/uk/webful/passwordmaker/MainActivity.java
+++ android/app/src/main/java/uk/webful/passwordmaker/MainActivity.java
@@ -1,5 +1,13 @@
 package uk.webful.passwordmaker;
 
+import android.os.Bundle;
+import com.bkon.capacitor.DarkMode.DarkMode;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(DarkMode.class);
+    }
+}
