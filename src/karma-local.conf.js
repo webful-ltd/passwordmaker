124f71209f872222a1f008de97825d2211e72bf2
Noel Light-Hilary <noel@noellh.com>
2022-03-26 16:59:45 +0000

    Use puppeteer to make CI tests headless
    Also remove a config workaround for a now-fixed Karma bug

--- src/karma-local.conf.js
+++ src/karma-local.conf.js
@@ -13,11 +13,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      // clearContext: false // leave Jasmine Spec Runner output visible in browser
-      // flip for now as we still seem to be getting a warning about page refreshes with
-      // it false even as of Karma v6.0.3. Issue https://github.com/karma-runner/karma/issues/3560
-      // suggests v6.x branch contains a fix.
-      clearContext: true
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, 'coverage'),
