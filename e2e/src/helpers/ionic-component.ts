39bd20f8721029c744eb5b7fbeb8ce2f4e03b794
Noel Light-Hilary <noel@noellh.com>
2024-08-25 13:39:23 +0100

    Remove weird, superfluous type hedging on IonicComponent `selector`

--- e2e/src/helpers/ionic-component.ts
+++ e2e/src/helpers/ionic-component.ts
@@ -1,15 +1,11 @@
 export class IonicComponent {
-  constructor(public selector: string | WebdriverIO.Element) {
+  constructor(public selector: string) {
   }
 
   get $() {
     return async ({ IonicPage }) => {
-      if (typeof this.selector === 'string') {
-        const activePage = await IonicPage.active();
-        return activePage.$(this.selector);
-      }
-
-      return this.selector;
+      const activePage = await IonicPage.active();
+      return activePage.$(this.selector);
     };
   }
 }
