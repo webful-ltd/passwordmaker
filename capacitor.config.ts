18fd6b31d963b237c9a684bc9e93f2764969f64b
Noel Light-Hilary <noel@noellh.com>
2025-07-03 22:37:12 +0100

    Fix statusbar position on Android; `npm update` & sync projects

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -20,6 +20,15 @@ const config: CapacitorConfig = {
       enable: true,
       imageName: 'Splashscreen',
     },
+    StatusBar: {
+      // Work around title position issues with Android API 35 for now.
+      // See https://github.com/ionic-team/ionic-framework/issues/30090#issuecomment-2708500924
+      // See also discussion & alternatives at https://github.com/ionic-team/capacitor/issues/7804 and
+      // https://github.com/ionic-team/ionic-framework/issues/30090
+      // Possibly consider @capawesome/capacitor-android-edge-to-edge-support if we find issues
+      // beyond the statusbar.
+      overlaysWebView: false,
+    }
   },
   server: {
     androidScheme: 'http', // https://capacitorjs.com/docs/updating/5-0#update-androidscheme
